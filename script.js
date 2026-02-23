6a9ab5d0812739dce947593d63c9f8f12157a48c
Avinash Kushwaha <avinashkushwaha7265@gmail.com>
2026-02-23 15:25:56 +0530

    ternary operator

--- script.js
+++ script.js
@@ -20,21 +20,21 @@ x = 20     //Redeclaration is not allowed in let, But re-declaration is not allo
 
 
 
-var num = 10;
-console.log(num)
-console.log(typeof(num))
-num = false
-console.log(num)
-console.log(typeof(num))
-num = "Avinash Kushwaha"
-console.log(num)
-console.log(typeof(num))
-num = null
-console.log(num)
-console.log(typeof(num))
-num = undefined
-console.log(num)
-console.log(typeof(num))
+// var num = 10;
+// console.log(num)
+// console.log(typeof(num))
+// num = false
+// console.log(num)
+// console.log(typeof(num))
+// num = "Avinash Kushwaha"
+// console.log(num)
+// console.log(typeof(num))
+// num = null
+// console.log(num)
+// console.log(typeof(num))
+// num = undefined
+// console.log(num)
+// console.log(typeof(num))
 
 /* 
 Refrence(object) type -->
@@ -65,22 +65,49 @@ Symbol type: New Primitive data type introduced in ES6d
 */
 
 // {/* <script type = "text/javascript"></script> */}
-const sym1 = Symbol(4)
-console.log(sym1)
-const sym2 = Symbol(4)
-console.log(sym2)
-if(sym1==sym2){
-    console.log("true")
-}
-else{
-    console.log("false") //Because Each symbol is different
-}
+// const sym1 = Symbol(4)
+// console.log(sym1)
+// const sym2 = Symbol(4)
+// console.log(sym2)
+// if(sym1==sym2){
+//     console.log("true")
+// }
+// else{
+//     console.log("false") //Because Each symbol is different
+// }
 
 var a =10
 var b = 13
 var c = 10
-var linebreak = "<br>"
-document.write(a+b+c)
-result = a+b+c
-document.write(result)
-document.write(linebreak)
+// var linebreak = "<br>"
+// document.write(a+b+c)
+// result = a+b+c
+// document.write(result)
+// document.write(linebreak)
+
+a = 10
+b = 20
+// document.write("(a<b>) => ")
+// result = (a<b)
+// document.write(result)
+// document.write(linebreak)
+
+// document.write("(a>b) => ")
+// result = (a>b);
+// document.write(result)
+
+//Ternary Opeartor --> variablename = (condition)?value1:value2;
+
+var age = 21
+var voteable = (age<18) ? console.log("Too young"):console.log("Young enough");
+
+
+var count;
+document.write("Starting loop" + "<br/>")
+
+for(count =0; count<10; count++){
+    document.write("Current Count: "+count)
+    document.write("<br/>")
+}
+
+document.write("Loop Stopped!")
